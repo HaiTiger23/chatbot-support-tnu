b85914dc0dfa994979d09c20a46204e194522c84
Tran Hai <haitv.dev@gmai.com>
2023-08-14 18:33:11 +0700

    update

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN
+
 let getHomePage = (req, res) => {
     return res.send("Home Page")
 };
 let getWebhook = (req, res) => {
-    let VERIFY_TOKEN = process.env.VERIFY_TOKEN
+    
     // Parse the query params
     let mode = req.query["hub.mode"];
     let token = req.query["hub.verify_token"];
@@ -31,6 +34,9 @@ let postWebhook = (req, res) => {
         body.entry.forEach(entry => {
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
         });
         // Returns a '200 OK' response to all requests
         res.status(200).send("EVENT_RECEIVED");
@@ -40,7 +46,19 @@ let postWebhook = (req, res) => {
         res.sendStatus(404);
       }
 }
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
 
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+}
 export default {
     getHomePage,
     getWebhook,
