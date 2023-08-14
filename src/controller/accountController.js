5b4bb9dd31363520e3087b1fdc25256aed723715
Tran Hai <haitv.dev@gmai.com>
2023-08-14 23:33:30 +0700

    cập nhật

--- src/controller/accountController.js
+++ src/controller/accountController.js
@@ -1,9 +1,19 @@
 const addAccount = function(message,user) {
-    let {SvID, password} = message.split('|');
-    return {
-        text:   "SVID: " + SvID + "\n" +
-                "Password: " + password
+    try {
+        let [SvID, password] = message.split('|');
+        SvID = SvID.trim()
+        password = SvID.trim()
+        return {
+            text:   "SVID: " + SvID + "\n" +
+                    "Password: " + password
+        }
+    } catch (e) {
+        console.log(e);
+        return {
+            text:  "Lỗi thử lại sau..."
+        }
     }
+   
 }
 
 export default {
